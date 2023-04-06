e70c2c84b88e7107cdc83fec9e5802a9ce81bff2
Marcelo Ceolin <fxsobr@gmail.com>
2023-04-06 20:40:25 -0300

    adicionado lib pbkdf2

--- mix.exs
+++ mix.exs
@@ -44,7 +44,8 @@ defmodule Delivery.MixProject do
       {:gettext, "~> 0.20"},
       {:jason, "~> 1.2"},
       {:plug_cowboy, "~> 2.5"},
-      {:credo, "~> 1.7", only: [:dev, :test], runtime: false}
+      {:credo, "~> 1.7", only: [:dev, :test], runtime: false},
+      {:pbkdf2_elixir, "~> 2.0"}
     ]
   end
 
