c8be07077361543238e59274a8e2692a487a9b07
Marcelo Ceolin <fxsobr@gmail.com>
2023-04-08 15:00:41 -0300

    criado delete de usuario

--- lib/delivery.ex
+++ lib/delivery.ex
@@ -1,7 +1,9 @@
 defmodule Delivery do
   alias Delivery.Users.Create, as: UserCreate
+  alias Delivery.Users.Delete, as: UserDelete
   alias Delivery.Users.Get, as: UserGet
 
   defdelegate create_user(params), to: UserCreate, as: :call
+  defdelegate delete_user(id), to: UserDelete, as: :call
   defdelegate get_user_by_id(id), to: UserGet, as: :by_id
 end
