7efd77290fefd90184ea8bd6a4a5fda0fe8e2efa
Marcelo Ceolin <fxsobr@gmail.com>
2023-04-08 15:17:03 -0300

    criado struct para lidar com os erros

--- lib/delivery_web/controllers/fallback_controller.ex
+++ lib/delivery_web/controllers/fallback_controller.ex
@@ -1,9 +1,9 @@
 defmodule DeliveryWeb.FallbackController do
   use DeliveryWeb, :controller
-
+  alias Delivery.Error
   alias DeliveryWeb.ErrorJSON
 
-  def call(conn, {:error, %{status: status, result: result}}) do
+  def call(conn, {:error, %Error{status: status, result: result}}) do
     conn
     |> put_status(status)
     |> put_view(ErrorJSON)
